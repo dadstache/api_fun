2cdf57be8f47720d9a5640440fef7174df2d2eb1
Darrick <6200177+dadstache@users.noreply.github.com>
2018-04-17 10:16:54 -0700

    Update mixpanel_2.py
    updated comments

--- mixpanel_2.py
+++ mixpanel_2.py
@@ -1,15 +1,19 @@
 import requests, base64, json
 
-####3 pieces to the puzzle
-#### pass the headers with the Mixpanel token
-#### encode the payload
-#### send encoded payload through a POST request to the HTTP API endpoint
+#### Segment & Mixpanel HTTP API
+#### A few steps are involved:
+#### 1) Implement Mixpanel token so requests are authenticated
+#### 2) Setup the payload with the proper parameters
+#### 3) Convert the payload from a dictionary to a json string so it can be encoded
+#### 4) Encode the payload json to base64
+#### 5) Build and send the the POST request with valid data parameters (url + ?data= + encoded payload) 
 
-##Mixpanel HTTP API
+#Mixpanel HTTP API Endpoint
 url = 'http://api.mixpanel.com/track/'
 token = 'INSERT HERE'
 headers = {'token' : token, 'Content-Type' : 'application/json'}
 
+#Payload with required token
 payload = {
   "userId": "user1",
   "event": "Product Viewed",
@@ -27,15 +31,15 @@ payload = {
   }
 }
 
-#convert dict to a string so it can be encoded
+#convert dict to a json string so it can be encoded
 payload_json = json.dumps(payload)
 print(payload_json)
 
-#encode the payload and print
+#encode the payload and verify
 encoded_data = base64.b64encode(payload_json)
 print("Here is the encoded payload: " + encoded_data)
 
-#Connect the final URL and POST
+#build the final url with data params and POST
 r = requests.post(url + '?data=' + encoded_data, headers = headers)
 if r.text == 1:
     print("Success your POST has been sent to mixpanel!")
